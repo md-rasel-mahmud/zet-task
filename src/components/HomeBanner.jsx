2c59e3c09181daeb99b16c3723c920b02c2fed81
myself-rasel-mahmud <md.rasel.mahmud.mail@gmail.com>
2023-06-18 09:41:59 +0600

    make website responsive

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -4,22 +4,24 @@ import playstoreImg from "../assets/plastoreIcon.svg";
 
 const HomeBanner = () => {
   return (
-    <div>
-      <div className="flex justify-between bg-[#e9f4ff] h-[360px] my-[110px] mb-auto items-end">
-        <div className="p-14 w-1/2 flex flex-col items-start h-full justify-center gap-3">
+    <div className="px-4 lg:px-0">
+      <div className="flex flex-col-reverse rounded md:flex-row justify-between bg-[#e9f4ff] lg:h-[360px] my-10 lg:my-[110px] w-full lg:mx-auto mb-auto items-end">
+        <div className="p-6 text-center lg:text-left lg:p-14 w-full mx-auto lg:w-1/2 flex flex-col items-start h-full justify-center gap-3">
           <h1 className="text-4xl leading-snug font-bold text-black">
             Become a Financial Advisor and{" "}
             <span className="text-primary">Earn Rs.1 Lakh/Month</span>
           </h1>
           <p className="text-xl text-black">No investment required</p>
 
+          <div className="w-full lg:w-fit">
             <Image
-            className="w-28 ml-5"
+              className="w-32 mx-auto lg:ml-5"
               src={playstoreImg}
               alt="Playstore Image"
             ></Image>
+          </div>
         </div>
-        <div>
+        <div className="hidden lg:flex">
           <Image
             className="w-[32vw] mr-8"
             src={bannerImg}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,50 +9,88 @@ const Navbar = () => {
   // create active link class name
   const router = usePathname();
 
+  const navItems = (
+    <>
+      <li>
+        <Link
+          className={router === "/" ? "text-primary focus:text-primary" : " "}
+          href={"/"}
+        >
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link
+          className={
+            router === "/about" ? "text-primary focus:text-primary" : ""
+          }
+          href={"/about"}
+        >
+          About
+        </Link>
+      </li>
+      <li>
+        <Link
+          className="bg-secondary text-white "
+          href={"https://play.google.com/store/apps/details?id=in.onecode.app"}
+        >
+          Download Zet
+        </Link>
+      </li>
+    </>
+  );
+
   return (
-    <div className="border-b-[1.9px]">
-      <div className="navbar max-w-screen-lg mx-auto px-0">
-        <div className="flex-1">
-          <Link href={"/"}>
-            <Image src={logo} alt="logo" width={90} height={90}></Image>
-          </Link>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal gap-3 text-lg px-1">
-            <li>
-              <Link
-                className={
-                  router === "/" ? "text-primary focus:text-primary" : " "
-                }
-                href={"/"}
+    <>
+      <div className="border-b-[1.9px]">
+        {/* <div className="navbar max-w-screen-lg mx-auto px-0">
+          <div className="flex-1">
+            <Link href={"/"}>
+              <Image src={logo} alt="logo" width={90} height={90}></Image>
+            </Link>
+          </div>
+          <div className="flex-none">
+            <ul className="menu menu-horizontal gap-3 text-lg px-1"></ul>
+          </div>
+        </div> */}
+        <div className="navbar max-w-screen-lg mx-auto px-2 lg:px-0">
+          <div className="navbar-start justify-between lg:justify-stretch w-full flex-row-reverse lg:flex-row">
+            <div className="dropdown dropdown-left">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               >
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={
-                  router === "/about" ? "text-primary focus:text-primary" : ""
-                }
-                href={"/about"}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="bg-secondary text-white "
-                href={
-                  "https://play.google.com/store/apps/details?id=in.onecode.app"
-                }
-              >
-                Download Zet
-              </Link>
-            </li>
-          </ul>
+                {navItems}
+              </ul>
+            </div>
+
+            <Link href={"/"}>
+              <Image className="pl-2 lg:px" src={logo} alt="logo" width={90} height={90}></Image>
+            </Link>
+          </div>
+
+          <div className="navbar-end hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{navItems}</ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
